d26920475a5a354a268ab7e9f2cedb10368d1f17
Stephen Clemmer <stephenjclemmer@gmail.com>
2022-07-18 19:02:24 -0700

    Added random customer generator to all locations.

--- js/app.js
+++ js/app.js
@@ -1,5 +1,11 @@
 'use strict';
 
+
+// random number generator function grabbed from MDN docs
+function randomCustPerHour(min, max){
+  return Math.floor(Math.random() * (max - min +1) + min);
+}
+
 let seattle = {
   hours: ['6am', '7am', '8am', '9am', '10am', '11am', '12am', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm'],
   minCust: 23,
@@ -11,13 +17,23 @@ let seattle = {
   }
 };
 
+seattle.getCustPerHour();
 console.log(seattle);
+
+
+
+
+
 
 let tokyo = {
   hours: ['6am', '7am', '8am', '9am', '10am', '11am', '12am', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm'],
   minCust: 3,
   maxCust: 24,
   avgCookieSale: 1.2,
+  custPerHour: 0,
+  getCustPerHour: function(){
+    this.custPerHour = `${randomCustPerHour(this.minCust, this.maxCust)}`;
+  }
 };
 
 let dubai = {
@@ -25,6 +41,10 @@ let dubai = {
   minCust: 11,
   maxCust: 38,
   avgCookieSale: 3.7,
+  custPerHour: 0,
+  getCustPerHour: function(){
+    this.custPerHour = `${randomCustPerHour(this.minCust, this.maxCust)}`;
+  }
 };
 
 let paris = {
@@ -32,6 +52,10 @@ let paris = {
   minCust: 20,
   maxCust: 38,
   avgCookieSale: 2.3,
+  custPerHour: 0,
+  getCustPerHour: function(){
+    this.custPerHour = `${randomCustPerHour(this.minCust, this.maxCust)}`;
+  }
 };
 
 let lima = {
@@ -39,9 +63,8 @@ let lima = {
   minCust: 2,
   maxCust: 16,
   avgCookieSale: 4.6,
+  custPerHour: 0,
+  getCustPerHour: function(){
+    this.custPerHour = `${randomCustPerHour(this.minCust, this.maxCust)}`;
+  }
 };
-
-// grabbed from MDN docs
-function randomCustPerHour(min, max){
-  return Math.floor(Math.random() * (max - min +1) + min);
-}
