ea18f66f410c1313df40db611506d495a250e10e
Stephen Clemmer <stephenjclemmer@gmail.com>
2022-07-18 22:28:52 -0700

    Question 3 Complete.

--- js/app.js
+++ js/app.js
@@ -1,26 +1,46 @@
 'use strict';
 
-
 // random number generator function grabbed from MDN docs
 function randomCustPerHour(min, max){
   return Math.floor(Math.random() * (max - min +1) + min);
 }
+
 
 let seattle = {
   hours: ['6am', '7am', '8am', '9am', '10am', '11am', '12am', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm'],
   minCust: 23,
   maxCust: 65,
   avgCookieSale: 6.3,
-  custPerHour: 0,
+
+  cookiesSoldPerHour: [],
+
   getCustPerHour: function(){
-    this.custPerHour = `${randomCustPerHour(this.minCust, this.maxCust)}`;
-  }
-};
+    this.custPerHour = randomCustPerHour(this.minCust, this.maxCust);
+    console.log(this.custPerHour);
+  },
+
+  getCookiesSoldPerHour: function(){
+    for(let i = 0; i <= this.hours.length; i++){
+      this.getCustPerHour();
+      this.cookiesSoldPerHour.push(this.custPerHour * this.avgCookieSale);
+    }
+
+  //  console.log(this.cookiesSoldPerHour);
+  },
+
+}
+
+
+
+// seattle.cookiesSoldPerHour();
 
 seattle.getCustPerHour();
+seattle.getCookiesSoldPerHour();
 console.log(seattle);
 
-
+console.log(seattle.avgCookieSale);
+console.log(seattle.custPerHour);
+// console.log(seattle.custPerHour * seattle.avgCookieSale);
 
 
 
@@ -32,8 +52,17 @@ let tokyo = {
   avgCookieSale: 1.2,
   custPerHour: 0,
   getCustPerHour: function(){
-    this.custPerHour = `${randomCustPerHour(this.minCust, this.maxCust)}`;
-  }
+    this.custPerHour = randomCustPerHour(this.minCust, this.maxCust);
+  },
+
+  getCookiesSoldPerHour: function(){
+    for(let i = 0; i <= this.hours.length; i++){
+      this.getCustPerHour();
+      this.cookiesSoldPerHour.push(this.custPerHour * this.avgCookieSale);
+    }
+
+  //  console.log(this.cookiesSoldPerHour);
+  },
 };
 
 let dubai = {
@@ -43,8 +72,17 @@ let dubai = {
   avgCookieSale: 3.7,
   custPerHour: 0,
   getCustPerHour: function(){
-    this.custPerHour = `${randomCustPerHour(this.minCust, this.maxCust)}`;
-  }
+    this.custPerHour = randomCustPerHour(this.minCust, this.maxCust);
+  },
+
+  getCookiesSoldPerHour: function(){
+    for(let i = 0; i <= this.hours.length; i++){
+      this.getCustPerHour();
+      this.cookiesSoldPerHour.push(this.custPerHour * this.avgCookieSale);
+    }
+
+  //  console.log(this.cookiesSoldPerHour);
+  },
 };
 
 let paris = {
@@ -54,8 +92,17 @@ let paris = {
   avgCookieSale: 2.3,
   custPerHour: 0,
   getCustPerHour: function(){
-    this.custPerHour = `${randomCustPerHour(this.minCust, this.maxCust)}`;
-  }
+    this.custPerHour = randomCustPerHour(this.minCust, this.maxCust);
+  },
+
+  getCookiesSoldPerHour: function(){
+    for(let i = 0; i <= this.hours.length; i++){
+      this.getCustPerHour();
+      this.cookiesSoldPerHour.push(this.custPerHour * this.avgCookieSale);
+    }
+
+  //  console.log(this.cookiesSoldPerHour);
+  },
 };
 
 let lima = {
@@ -65,6 +112,37 @@ let lima = {
   avgCookieSale: 4.6,
   custPerHour: 0,
   getCustPerHour: function(){
-    this.custPerHour = `${randomCustPerHour(this.minCust, this.maxCust)}`;
-  }
+    this.custPerHour = randomCustPerHour(this.minCust, this.maxCust);
+  },
+
+  getCookiesSoldPerHour: function(){
+    for(let i = 0; i <= this.hours.length; i++){
+      this.getCustPerHour();
+      this.cookiesSoldPerHour.push(this.custPerHour * this.avgCookieSale);
+    }
+
+  //  console.log(this.cookiesSoldPerHour);
+  },
 };
+
+
+
+
+
+//   let liElem = createElement('li');
+//   liElem.textContent = `${[i]}: ${cookiesSoldPerHour()}`
+// }
+    
+
+
+// render: function(){
+
+// }
+// let ulElem = document.createElement('ul');
+// articleElem.appendChild(ulElem);
+
+// for(let i = 0; i < this.interests.length; i++){
+//   let liElem = document.createElement('li');
+//   liElem.textContent = this.interests[i];
+//   ulElem.appendChild(liElem);
+// }
